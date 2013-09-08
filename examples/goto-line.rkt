fe70ead37c15e677bbc790fb8b04abd3df23084a
Laurent Orseau <laurent.orseau@gmail.com>
2013-09-08 17:30:03 +0200

    re-fix line checking + current line as default line

--- examples/goto-line.rkt
+++ examples/goto-line.rkt
@@ -5,11 +5,17 @@
 ;; string? -> (or/c string? #f)
 (provide item-callback)
 (define (item-callback str #:editor ed) 
-  (define line (get-text-from-user "Goto line" "Line number:"
-                                   #:validate string->number))
-  (when line
+  (define line-str (get-text-from-user "Goto line" "Line number:"
+                                       #f
+                                       (number->string 
+                                        (add1
+                                         (send ed position-paragraph 
+                                               (send ed get-end-position))))
+                                       #:validate string->number))
+  (define line (and line-str (string->number line-str)))
+  (when (exact-nonnegative-integer? line)
     (send ed set-position (send ed paragraph-start-position
-                                (sub1 (string->number line)))))
+                                (sub1 line))))
   #f)
 
 ;; See the manual in the Script/Help menu for more information.
