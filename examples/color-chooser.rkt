01420d2fbf29b2a6d41171e4ceb5744cd495396b
Laurent Orseau <laurent.orseau@gmail.com>
2013-09-08 12:31:53 +0200

    color-chooser recognizes more color forms

--- examples/color-chooser.rkt
+++ examples/color-chooser.rkt
@@ -1,22 +1,32 @@
 #lang racket/base
-(require racket/gui/base racket/class racket/match)
+(require racket/gui/base 
+         racket/class
+         racket/match
+         racket/port)
 
 ;;; Laurent Orseau <laurent orseau gmail com> -- 2012-04-21
 
 ;; string? -> (or/c string? #f)
 (provide item-callback)
 (define (item-callback str)
-  (define c-old 
-    (match (read (open-input-string str))
-      [(list 'make-object 'color% r g b)
-       (make-object color% r g b)]
-      [else #f]))
-  (let ([c (get-color-from-user #f #f c-old)])
+  (define-values
+    (r g b new-str)
+    (match (port->list read (open-input-string str))
+      [`((make-object color% ,(? number? r) ,(? number? g) ,(? number? b)))
+       (values r g b "(make-object color% ~a ~a ~a)")]
+      [`((make-color ,(? number? r) ,(? number? g) ,(? number? b)))
+       (values r g b "(make-color ~a ~a ~a)")]
+      [`(,(? number? r) ,(? number? g) ,(? number? b))
+       (values r g b "~a ~a ~a")]
+      [else (values #f #f #f "(make-color ~a ~a ~a)")]))
+  (let ([c (get-color-from-user #f #f (and r (make-color r g b)))])
     (and c
-         (format "(make-object color% ~a ~a ~a)"
+         (format new-str
                  (send c red)
                  (send c green)
                  (send c blue)))))
 
 ; Select the following s-exp and click on the color-chooser script menu item:
-; (make-object color% 77 156 161)
+; (make-object color% 90 158 163)
+; 65 65 156
+; (make-color 142 170 199)
